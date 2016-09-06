d0e8930ecf6307afa6291123dc3fe1091f08ebbe
sidazhou <sidazhou@gmail.com>
2016-09-06 11:42:59 -0700

    massaging some details, only twitter api part is missing due to CORS / authentication

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -2,13 +2,14 @@ $(function() {
 
 // http://stackoverflow.com/questions/4613310/how-to-call-external-url-in-jquery
   $('#twitter_search_button').click(function(){
-    var query = $('#twitter_search_query').value;
+    var query = $('#twitter_search_query').val();
 
     $.ajax({
       url: "/show?query=" + query,
       type: 'GET',
       success: function(data) {
-        $('div#response').append("sdajax success");
+        console.log(data);
+        $('div#response').append(data["test"]);
       },
       error: function(response) {
         $('div#response').append("sdajax error");
--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,14 +2,13 @@ class TwitterController < ApplicationController
   def index
   end
 
-  def show
-    # https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=sidazhou&count=1
+  def show #should be in api
     # doesnt work
-    @response = JSON.parse(RestClient.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=sidazhou&count=1"))
+    # @response = JSON.parse(RestClient.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=sidazhou&count=1"))
 
     respond_to do |format|
       format.json do
-        render json: { test: 123 }
+        render json: { test: params[:query] }
       end
     end
   end
