67bdcf21e91ee69d75fc7a264ff06c1643676568
sidazhou <sidazhou@gmail.com>
2016-09-06 11:15:48 -0700

    added restclient

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -1,15 +1,31 @@
 $(function() {
+
+// http://stackoverflow.com/questions/4613310/how-to-call-external-url-in-jquery
+var twitterURL="https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=sidazhou";
+$.ajax({
+    url: twitterURL+"&callback=?",
+    type: 'GET',
+    success: function (resp) {
+        alert(resp);
+    },
+    error: function(e) {
+        alert('Error: '+e);
+    }
+});
+
+
   $('#twitter_search_button').click(function(){
     $.ajax({
       // url: '/welcome',
       // data: { data: data },
       method: 'GET',
       success: function(data) {
-        $('div.response').append("sdajax success");
+        $('div#response').append("sdajax success");
       },
       error: function(response) {
-        $('div.response').append("sdajax error");
+        $('div#response').append("sdajax error");
       }
     });
   });
 });
+
--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,4 +1,8 @@
 class TwitterController < ApplicationController
   def index
   end
+
+  def show
+    @response = JSON.parse(RestClient.get(mailgun_message_url))
+  end
 end
