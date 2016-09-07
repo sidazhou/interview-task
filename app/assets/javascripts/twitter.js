5411d278b3ef585b189f129670729f21f5d8f0ea
sidazhou <sidazhou@gmail.com>
2016-09-06 18:22:51 -0700

    added display tweets, added filter

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -1,20 +1,41 @@
+var tweets; // global var
+var tweets_shown; // global var
+
 $(function() {
+  query_twitter('salesforce');
 
-// http://stackoverflow.com/questions/4613310/how-to-call-external-url-in-jquery
-  $('#twitter_search_button').click(function(){
-    var query = $('#twitter_search_query').val();
+  $('#tweet_filter_query').keyup(function(){
+    var str_filter_query = $('#tweet_filter_query').val()
+    tweets_shown = _.filter( tweets, function(el) { return el.text.includes(str_filter_query) })
+    update_response(tweets_shown);
+  })
 
-    $.ajax({
-      url: "/show?query=" + query,
-      type: 'GET',
-      success: function(data) {
-        console.log(data);
-        $('div#response').html(data["tweet"]);
-      },
-      error: function(response) {
-        $('div#response').html("user " + query + " not found");
-      }
-    });
-  });
 });
 
+var query_twitter = function(str_query) {
+  $.ajax({
+    url: "/show?query=" + str_query,
+    type: 'GET',
+    success: function(data) {
+      tweets = _.first(data.tweets, 10)
+      tweets_shown = tweets
+
+      update_response(tweets_shown);
+    },
+    error: function(response) {
+      update_response(null);
+    }
+  });
+}
+
+var update_response = function(tweets) {
+  // if error
+  // $('div#response').html("user " + str_query + " not found");
+  // return
+
+  var html_tweets = tweets.map( function(tweet) {
+     return "<p>" + tweet.text + "</p>";
+  })
+
+  $('div#response').html(html_tweets);
+}
--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,8 +2,7 @@ class TwitterController < ApplicationController
   def index
   end
 
-  def show #should be in api
-    #should be put somewhere else
+  def show
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "NFlQOaQspcPdOBHbXOI2KuAIN"
       config.consumer_secret     = "5D6mzFQGK3oxN3rDzHZwHsST2kofGcKRoCGZ7ZRnUgHZXP2V1s"
@@ -15,11 +14,20 @@ class TwitterController < ApplicationController
       format.json do
         begin
           tweets = client.user_timeline(params[:query])
-          render json: { tweet: tweets.first.text }
+          render json: { tweets: tweets }
         rescue
-          render json: { tweet: '' }, status: 500
+          render json: { tweets: '' }, status: 500
         end
       end
     end
   end
 end
+
+
+
+# .attrs[:user][:name]
+# .attrs[:user][:screen_name]
+# .attrs[:user][:profile_image_url]
+# .attrs[:text]
+# .attrs[:text]
+# .attrs[:retweet_count]
