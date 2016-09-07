d5f1e293598fbe401e3440ffa281f76bfddf3723
sidazhou <sidazhou@gmail.com>
2016-09-06 18:46:08 -0700

    added highlighting

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -1,16 +1,32 @@
 var tweets; // global var
 var tweets_shown; // global var
 
+
 $(function() {
+  // TODO: auto refresh at interval
   query_twitter('salesforce');
 
   $('#tweet_filter_query').keyup(function(){
-    var str_filter_query = $('#tweet_filter_query').val()
-    tweets_shown = _.filter( tweets, function(el) { return el.text.includes(str_filter_query) })
-    update_response(tweets_shown);
+    var str_filter_query = $('#tweet_filter_query').val();
+
+    tweets_shown = _.filter(tweets, function(el) { return el.text.includes(str_filter_query) })
+
+    var html_blob = highlight_html(build_html(tweets_shown), str_filter_query)
+    update_response(html_blob);
   })
 
 });
+
+
+var highlight_html = function(html_blob, highlight_str) {
+  if ( highlight_str=='' ) {
+    return html_blob;
+  }
+
+  var re = new RegExp(highlight_str, 'g'); // TODO: bugs out on \ and /
+
+  return html_blob.replace(re, "<span class='highlighted'>" + highlight_str + '</span>')
+}
 
 var query_twitter = function(str_query) {
   $.ajax({
@@ -20,22 +36,22 @@ var query_twitter = function(str_query) {
       tweets = _.first(data.tweets, 10)
       tweets_shown = tweets
 
-      update_response(tweets_shown);
+      update_response(build_html(tweets_shown));
     },
     error: function(response) {
-      update_response(null);
+      alert(response);
     }
   });
 }
 
-var update_response = function(tweets) {
-  // if error
-  // $('div#response').html("user " + str_query + " not found");
-  // return
-
-  var html_tweets = tweets.map( function(tweet) {
-     return "<p>" + tweet.text + "</p>";
-  })
-
-  $('div#response').html(html_tweets);
+var update_response = function(html_blob) {
+  $('div#response').html(html_blob);
 }
+
+var build_html = function (tweets) {
+  return tweets.map( function(tweet) {
+     return "<p>" + tweet.text + "</p>";
+  }).join('')
+}
+
+
